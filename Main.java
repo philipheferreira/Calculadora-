a0c99bec9af865eacd75df655d99fa047c58148b
philipheferreira <49068956+philipheferreira@users.noreply.github.com>
2021-02-02 15:01:27 -0300

    criando o main
    Main é o arquivo do código onde se encontra a função principal, responsável por plotar o arquivo CalculadoraJFrame.form

---
+++ Main.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+import Telas.CalculadoraJFrame;
+
+
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        new CalculadoraJFrame().setVisible(true);
+
+    }
+    
+}
